0a28e889258292de69592bb5d57206a6bcc6b6c5
alexroker <indigo-terra@yandex.ru>
2020-10-30 12:51:46 +0300

    Init

---
+++ scripts/_common.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="ffmpeg postgresql postgresql-contrib openssl g++ redis-server redis-tools mailutils apt-transport-https"
+
+YNH_NODEJS_VERSION=12
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
